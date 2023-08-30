0ed06b16c5077fa0cfb739c6f057967ae5900e02
Aniket chaturvedi <aniketchaturvedi309@gmail.com>
2023-08-30 10:15:49 +0530

    30/08/2023

---
+++ (E) Largest Element in the Array .cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h> 
+int largestElement(vector<int> &arr, int n) {
+    int maxi=INT_MIN;
+    for(int i=0;i<arr.size();i++){
+        maxi=max(maxi,arr[i]);
+
+    }
+    return maxi;
+}
